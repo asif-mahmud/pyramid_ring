95909a90139ee3b32188961b6cf88211e83b39ea
Asif Mahmud Shimon <ams.eee09@gmail.com>
2016-12-28 22:46:27 +0600

    v2.0.0 initial

---
+++ pyramid_ring/scaffolds/pyramid_ring/karma.conf.js
@@ -0,0 +1,4 @@
+/**
+ * Created by shimon on 12/28/16.
+ */
+module.exports = require('./config/karma.conf.js');
---
+++ pyramid_ring/scaffolds/pyramid_ring/webpack.config.js
@@ -0,0 +1,4 @@
+/**
+ * Created by shimon on 12/28/16.
+ */
+module.exports = require('./config/webpack.dev.js');
