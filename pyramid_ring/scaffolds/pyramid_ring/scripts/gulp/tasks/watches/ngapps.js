8313cfd45640b1bb5950bb07846975dd7102204e
Asif Mahmud Shimon <ams.eee09@gmail.com>
2017-01-02 20:30:14 +0600

    v2.0.0 workout

--- pyramid_ring/scaffolds/pyramid_ring/scripts/gulp/tasks/watches/ngapps.js
+++ pyramid_ring/scaffolds/pyramid_ring/scripts/gulp/tasks/watches/ngapps.js
@@ -2,6 +2,10 @@
  * Created by shimon on 1/2/17.
  */
 var gulp = require('gulp');
+var watch = require('gulp-watch');
+var helper = require('../../../helpers.js');
+var runSequence = require('run-sequence');
 
 gulp.task('watch:ngapps', function () {
+    gulp.watch(helper.root('ngapps', 'home/**'), ['build:homeApp']);
 });
